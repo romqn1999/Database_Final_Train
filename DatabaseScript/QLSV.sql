d5cee266e5c91e8d2a27a28c83acd0987cfc03bd
OtterB8 <caberrrr@gmail.com>
2019-05-29 09:59:53 +0700

    Add DatabaseScript folder with basic database creation

---
+++ DatabaseScript/QLSV.sql
@@ -0,0 +1,69 @@
+create database QLSV
+go
+
+use QLSV
+go
+
+create table sinhvien
+(
+	mssv char(10) not null,
+	hoten nvarchar(50),
+	ngaysinh date,
+	quequan nvarchar(50)
+)
+
+create table taikhoan
+(
+	id char(10) not null,
+	pass char(30)
+)
+
+create table lop
+(
+	malop char(10) not null
+)
+
+create table monhoc
+(
+	mamon char(10) not null,
+	tenmonhoc nvarchar(50)
+)
+
+create table lop_monhoc
+(
+	malop char(10) not null,
+	mamon char(10) not null
+)
+
+create table dangky
+(
+	malop char(10) not null,
+	mamon char(10) not null,
+	mssv char(10) not null
+)
+
+
+alter table sinhvien add
+	constraint pk_sinhvien primary key (mssv)
+
+alter table taikhoan add
+	constraint pk_taikhoan primary key (id),
+	constraint fk_taikhoan_id foreign key (id) references sinhvien(mssv)
+
+alter table lop add
+	constraint pk_lop primary key (malop)
+
+alter table monhoc add
+	constraint pk_monhoc primary key (mamon)
+
+alter table lop_monhoc add
+	constraint pk_lop_monhoc primary key (malop, mamon),
+	constraint fk_lop_monhoc_malop foreign key (malop) references lop(malop),
+	constraint fk_lop_monhoc_mamon foreign key (mamon) references monhoc(mamon)
+
+alter table dangky add
+	constraint pk_dangky primary key (malop, mamon, mssv),
+	constraint fk_dangky_malop foreign key (malop) references lop(malop),
+	constraint fk_dangky_mamon foreign key (mamon) references monhoc(mamon),
+	constraint fk_dangky_mssv foreign key (mssv) references sinhvien(mssv)
+
