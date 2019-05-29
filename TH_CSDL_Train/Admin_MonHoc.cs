22b34055701464118f8e02aafc24c91b1ebcfcf1
howlongforever <trandinhtonhieu2@gmail.com>
2019-05-29 09:37:29 +0700

    Initial commit

---
+++ TH_CSDL_Train/Admin_MonHoc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TH_CSDL_Train
+{
+    public partial class Admin_MonHoc : Form
+    {
+        public Admin_MonHoc()
+        {
+            InitializeComponent();
+        }
+    }
+}
